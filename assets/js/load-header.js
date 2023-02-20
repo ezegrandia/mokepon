f97a8827a17f25d53ce3786c7f66107699c7cee4
Ezegrandia <ezequielgrandia@gmail.com>
2023-02-20 04:08:58 -0300

    first commit

---
+++ assets/js/load-header.js
@@ -0,0 +1,6 @@
+// Carga el contenido del header.html y lo inserta en el elemento <header></header> del index.html
+fetch("./assets/components/header.html")
+    .then((response) => response.text())
+    .then((data) => {
+        document.getElementById("header").innerHTML = data;
+    });
