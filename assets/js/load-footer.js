4667c604886aff651b39542b210be243700c7470
Ezequiel Grandia Laptop <ezequielgrandia@gmail.com>
2023-02-21 20:17:05 -0300

    reorganizando funciones de load

--- assets/js/load-footer.js
+++
@@ -1,6 +0,0 @@
-// Carga el contenido del footer.html y lo inserta en el elemento <footer></footer> del index.html
-fetch("footer.html")
-    .then((response) => response.text())
-    .then((data) => {
-        document.getElementById("footer").innerHTML = data;
-    });
--- assets/js/load-help-panel.js
+++
@@ -1,6 +0,0 @@
-// Carga el contenido del help-panel.html y lo inserta en el elemento <aside></aside> del index.html
-fetch("./assets/components/help-panel.html")
-  .then((response) => response.text())
-  .then((data) => {
-    document.getElementById("help-panel").innerHTML = data;
-  });
