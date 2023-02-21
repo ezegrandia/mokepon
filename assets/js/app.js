5e24d56b8bb31fea6fd0734d237dbacd3c2c3470
Ezegrandia <ezequielgrandia@gmail.com>
2023-02-21 20:52:28 -0300

    arreglando bugs temporales

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,6 @@
 const signature = document.getElementById("signature");
 const audio = document.getElementById("audio");
 const audioBtn = document.getElementById("audio-btn");
-console.log(audioBtn);
 // const spanishBtn = document.getElementById("spanish-btn");
 // const englishBtn = document.getElementById("english-btn");
 const helpBtn = document.getElementById("help-btn");
@@ -23,55 +22,55 @@ let panelStatus = 0;
 // };
 
 function soundOnOf() {
-  if (audioCount == 0) {
-    audioCount = 1;
-    audioBtn.style.backgroundImage = "url(/mokepon/assets/img/mute-btn.svg)";
-    audio.play();
-  } else {
-    audioCount = 0;
-    audioBtn.style.backgroundImage = "url(/mokepon/assets/img/audio-btn.svg)";
-    audio.pause();
-  }
+    if (audioCount == 0) {
+        audioCount = 1;
+        audioBtn.style.backgroundImage = "url(/assets/img/mute-btn.svg)";
+        audio.play();
+    } else {
+        audioCount = 0;
+        audioBtn.style.backgroundImage = "url(/assets/img/audio-btn.svg)";
+        audio.pause();
+    }
 }
 
 function changeLang(btn) {
-  if (btn.id == "spanish-btn") {
-    logoImg.setAttribute("src", "./Assets/Mokepon-Logo-ES.svg");
-    subtitle.innerHTML = "Selecciona un personaje para comenzar el viaje!";
-    mainBtn.innerHTML = "Continuar";
-  } else {
-    logoImg.setAttribute("src", "./Assets/Mokepon-Logo-EN.svg");
-    subtitle.innerHTML = "Select a character to start the trip!";
-    mainBtn.innerHTML = "Continue";
-  }
+    if (btn.id == "spanish-btn") {
+        logoImg.setAttribute("src", "/assets/img/logo-es.svg");
+        subtitle.innerHTML = "Selecciona un personaje para comenzar el viaje!";
+        mainBtn.innerHTML = "Continuar";
+    } else {
+        logoImg.setAttribute("src", "/assets/img/logo-en.svg");
+        subtitle.innerHTML = "Select a character to start the trip!";
+        mainBtn.innerHTML = "Continue";
+    }
 }
 
 function openHelpPanel() {
-  helpPanel.classList.add("visible");
-  panelStatus = 1;
-  enableDisableBtns(panelStatus);
+    helpPanel.classList.add("visible");
+    panelStatus = 1;
+    enableDisableBtns(panelStatus);
 }
 
 function closeHelpPanel() {
-  helpPanel.classList.remove("visible");
-  panelStatus = 0;
-  enableDisableBtns(panelStatus);
+    helpPanel.classList.remove("visible");
+    panelStatus = 0;
+    enableDisableBtns(panelStatus);
 }
 
 function enableDisableBtns(panelStatus) {
-  if (panelStatus == 1) {
-    logoLink.removeAttribute("href");
-    logoImg.classList.remove("title-logo-img-hover");
-    signature.removeAttribute("href");
-    signature.classList.remove("signature-hover");
-    mainBtn.classList.remove("btn-hover");
-    cardsArr.forEach((card) => card.classList.remove("card-hover"));
-  } else if (panelStatus == 0) {
-    logoLink.setAttribute("href", "./mokepon.html");
-    logoImg.classList.add("title-logo-img-hover");
-    signature.setAttribute("href", "https://www.linkedin.com/in/ezegrandia/");
-    signature.classList.add("signature-hover");
-    mainBtn.classList.add("btn-hover");
-    cardsArr.forEach((card) => card.classList.add("card-hover"));
-  }
+    if (panelStatus == 1) {
+        logoLink.removeAttribute("href");
+        logoImg.classList.remove("title-logo-img-hover");
+        signature.removeAttribute("href");
+        signature.classList.remove("signature-hover");
+        // mainBtn.classList.remove("btn-hover");
+        // cardsArr.forEach((card) => card.classList.remove("card-hover"));
+    } else if (panelStatus == 0) {
+        logoLink.setAttribute("href", "./mokepon.html");
+        logoImg.classList.add("title-logo-img-hover");
+        signature.setAttribute("href", "https://www.linkedin.com/in/ezegrandia/");
+        signature.classList.add("signature-hover");
+        // mainBtn.classList.add("btn-hover");
+        // cardsArr.forEach((card) => card.classList.add("card-hover"));
+    }
 }
--- assets/js/load-content.js
+++ assets/js/load-content.js
@@ -1,41 +1,41 @@
 //Carga el contenido del header.html y lo inserta en el elemento <header></header> del index.html
 fetch("./assets/components/header.html")
-  .then((response) => response.text())
-  .then((data) => {
-    document.getElementById("header").innerHTML = data;
-  });
+    .then((response) => response.text())
+    .then((data) => {
+        document.getElementById("header").innerHTML = data;
+    });
 
 // Carga el contenido del help-panel.html y lo inserta en el elemento <aside></aside> del index.html
 fetch("./assets/components/help-panel.html")
-  .then((response) => response.text())
-  .then((data) => {
-    document.getElementById("help-panel").innerHTML = data;
-  });
+    .then((response) => response.text())
+    .then((data) => {
+        document.getElementById("help-panel").innerHTML = data;
+    });
 
 // Carga el contenido del logo.html y lo inserta en el elemento con el id="logo-section" del index.html
 fetch("./assets/components/logo.html")
-  .then((response) => response.text())
-  .then((data) => {
-    document.getElementById("logo-section").innerHTML = data;
-  });
-
-// Carga el contenido del footer.html y lo inserta en el elemento <footer></footer> del index.html
-fetch("footer.html")
-  .then((response) => response.text())
-  .then((data) => {
-    document.getElementById("footer").innerHTML = data;
-  });
-
-// Función para cargar una pantalla y cambiar el contenido del elemento main
-function loadPage(page) {
-  fetch(page)
     .then((response) => response.text())
     .then((data) => {
-      document.getElementById("main").innerHTML = data;
+        document.getElementById("logo-section").innerHTML = data;
     });
-}
+
+// Carga el contenido del footer.html y lo inserta en el elemento <footer></footer> del index.html
+// fetch("footer.html")
+//   .then((response) => response.text())
+//   .then((data) => {
+//     document.getElementById("footer").innerHTML = data;
+//   });
+
+// Función para cargar una pantalla y cambiar el contenido del elemento main
+// function loadPage(page) {
+//   fetch(page)
+//     .then((response) => response.text())
+//     .then((data) => {
+//       document.getElementById("main").innerHTML = data;
+//     });
+// }
 
 // Carga el contenido de la primera pantalla y lo inserta en el elemento main
-loadPage("page1.html");
+// loadPage("page1.html");
 
 //El resto de pantallas se cargan segun los diferentes buttons y eventos onclick
